6269dce4fc27d1e875cc7f41342710af746b5ee6
fatyflor <fatima.floresgalicia@cesunbc.edu.mx>
2025-10-11 16:50:48 -0700

    Add files via upload

---
+++ Equipo_2/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App de Películas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 7, 26, 235),
+        ),
+        useMaterial3: true,
+      ),
+      home: const LoadPage(),
+    );
+  }
+}
